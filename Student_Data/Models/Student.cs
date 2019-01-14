8b6a2cd7842017fab1f477817cf9ea303c2df02c
PradhumnaPancholi <pnpancholi6629.pnp@gmail.com>
2019-01-14 00:53:20 -0500

    basic setup, created 'Student' model class

---
+++ Student_Data/Models/Student.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Student_Data.Models
+{
+    public class Student
+    {
+        [Key]
+        public int StudentId { get; set; }
+
+        [Required]
+        public String FirstNAme { get; set; }
+
+        [Required]
+        public String LastName { get; set; }
+
+        [Required]
+        public String Address { get; set; }
+
+        [Required]
+        public int Phone { get; set; }
+
+        [Required]
+        public String Email { get; set; }
+
+    }
+}
